75ad396d07aa334c25785ad7bdd3eecad01878cb
TAKAHASHI Shuuji <shuuji3@gmail.com>
2021-12-20 01:03:58 +0900

    Add a debian vm

--- gcp/main.tf
+++ gcp/main.tf
@@ -18,3 +18,19 @@ provider "google" {
 resource "google_compute_network" "vpc_network" {
   name = "tf-net"
 }
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "e2-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {}
+  }
+}
